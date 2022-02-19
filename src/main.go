949de09ac2cda483dd3dba342ad437bc4d3e5acb
Shraddha Gajul <shraddha.gajul.27@gmail.com>
2022-02-19 22:33:25 +0530

    index/tags

--- src/main.go
+++ src/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/jinzhu/gorm"
 	_ "github.com/lib/pq"
 )
@@ -16,15 +18,34 @@ func main() {
 	// db.SingularTable(true)
 	db.CreateTable(&User{})
 
+	db.CreateTable(&Engineer{})
+
+	for _, f := range db.NewScope(&Engineer{}).Fields() {
+		fmt.Println(f.Name)
+	}
+
+	db.Model(&User{}).AddIndex("idx_first_name", "first_name")
+	db.Model(&User{}).AddUniqueIndex("idx_last_name", "LastName")
+
+	db.Model(&User{}).RemoveIndex("idx_first_name")
+
 }
 
 type User struct {
-	Id        uint
-	Username  string
-	FirstName string
-	LastName  string
+	// gorm.Model
+	UserId    int    `gorm:"primary_key"`
+	Username  string `sql:"type:VARCHAR(15);unique;unique_index; not null"`
+	FirstName string `sql:"size:150;DEFAULT:'Nobody'"`
+	LastName  string `gorm:"column:LastName"`
+	Count     int    `gorm:"AUTO_INCREMENT"`
+	TempField bool   `sql:"-"`
 }
 
-func (u User) TableName() string {
-	return "stakeholders"
+// func (u User) TableName() string {
+// 	return "stakeholders"
+// }
+
+type Engineer struct {
+	UserDetails User `gorm:"embedded"`
+	Degree string
 }
