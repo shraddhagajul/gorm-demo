89aec22520a06481c2ec43d0245a338ddc2df684
Shraddha Gajul <shraddha.gajul.27@gmail.com>
2022-02-22 13:45:07 +0530

    CRU- op

--- src/main.go
+++ src/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"fmt"
+	"time"
 
 	"github.com/jinzhu/gorm"
 	_ "github.com/lib/pq"
@@ -16,43 +16,50 @@ func main() {
 
 	db.DropTable(&User{})
 	db.CreateTable(&User{})
+	db.DropTable(&Appointment{})
+	db.CreateTable(&Appointment{})
 
-	users := make([]User, 0)
-
-	users = append(users, User{Username: "danny", FirstName: "Dan", LastName: "Morris"})
-	users = append(users, User{Username: "manny", FirstName: "Manuel", LastName: "Chris"})
-	users = append(users, User{Username: "john", FirstName: "John", LastName: "Doe"})
-
-	for _, user := range users {
-		db.Create(&user)
+	user := &User{Username: "joe_jonas", FirstName: "Joe", LastName: "Jonas", Salary: 1000}
+	user1 := &User{Username: "dylan_23", FirstName: "Dylan", LastName: "Lockhart", Salary: 2000}
+	appointments := []Appointment{
+		{Subject: "Concert Tour"},
+		{Subject: "Drive Tour"},
 	}
+	user.Appointments = appointments
 
-	firstUser := User{}
-	db.First(&firstUser)
-	fmt.Println(firstUser)
+	db.Debug().Create(&user)
+	db.Debug().Create(&user1)
 
-	lastUser := User{}
-	db.Last(&lastUser)
-	fmt.Println(lastUser)
+	// user.FirstName = "Shelly"
+	// user.LastName = "Gardner"
 
-	queryUser := User{Username: "manny"}
-	db.Where(&queryUser).First(&queryUser)
+	// db.Debug().Save(&user)
 
-	queryUser.LastName = "Chris N."
+	// db.Debug().Model(&user).Update("first_name", "Shelly")
 
-	db.Save(&queryUser)
-	fmt.Println("Query user ", queryUser)
-	fetchUpdatedUser := User{}
-	db.Where(&queryUser).First(&fetchUpdatedUser)
-	fmt.Println("Updated User : ", fetchUpdatedUser)
+	// db.Debug().Model(&user).Updates(map[string]interface{}{
+	// 	"first_name": "Shelly",
+	// 	"last_name": "Gardner",
+	// })
 
-	db.Where(&User{Username: "danny"}).Delete(&User{})
-	fmt.Println("done")
+	db.Debug().Table("users").Where("salary < 1500").Update("salary", gorm.Expr("salary + 200"))
+
 }
 
 type User struct {
-	Id        uint
-	Username  string
-	FirstName string
-	LastName  string
+	Id           uint
+	Username     string
+	FirstName    string
+	LastName     string
+	Salary uint
+	Appointments []Appointment
+}
+
+type Appointment struct {
+	gorm.Model
+	UserID      uint
+	Subject     string
+	Description string
+	StartTime   time.Time
+	Length      uint
 }
